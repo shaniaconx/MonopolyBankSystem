9bb8be9c37840dc667c6e4ae642f6c2b2f22ec70
shania <s.manso.2022@alumnos.urjc.es>
2023-12-11 15:13:03 +0100

    si

--- src/monopolybank/Terminal.java
+++ src/monopolybank/Terminal.java
@@ -3,7 +3,7 @@ package monopolybank;
 public class Terminal {
     Terminal(){};
 
-    public void read(){};
+    public int read(){};
     public void  show(String msg){};
     public TranslatorManager getTranslatorManager(){
         TranslatorManager t = new TranslatorManager();
